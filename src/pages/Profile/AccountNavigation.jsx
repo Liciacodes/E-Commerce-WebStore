4366eef202e7e715c62065075eb64bb557674864
Liciacodes <udosenfelicia@gmail.com>
2024-06-08 21:34:01 +0100

    Fixed the Account Navigation Page

--- src/pages/Profile/AccountNavigation.jsx
+++ src/pages/Profile/AccountNavigation.jsx
@@ -1,48 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { LuShoppingCart } from "react-icons/lu";
 import { FaRegHeart } from "react-icons/fa6";
-import { CiDeliveryTruck } from "react-icons/ci";
-import {
-  FaShoppingCart,
-  FaHeart,
-  FaMapMarkerAlt,
-  FaKey,
-  FaUser,
-  FaSignOutAlt,
-} from "react-icons/fa";
+// import { CiDeliveryTruck } from "react-icons/ci";
+// import { GrLogout } from "react-icons/gr";
+import { HiOutlineLogout } from "react-icons/hi";
+import { HiOutlineUser } from "react-icons/hi";
+import { RiKey2Line } from "react-icons/ri";
+import { GrDeliver } from "react-icons/gr";
+// import {
+//   FaShoppingCart,
+//   FaHeart,
+//   FaMapMarkerAlt,
+//   FaKey,
+//   FaUser,
+//   FaSignOutAlt,
+// } from "react-icons/fa";
 
 export default function AccountNavigation() {
+  const [activeLink, setActiveLink] = useState("orders");
+
+  const sections = [
+    {
+      key: "orders",
+      label: "Orders",
+      icon: <LuShoppingCart className="text-gray-600" size={24} />,
+    },
+    {
+      key: "wishlist",
+      label: "Wishlist",
+      icon: <FaRegHeart className="text-gray-600" size={24} />,
+    },
+    {
+      key: "address",
+      label: "Address",
+      icon: <GrDeliver className="text-gray-600" size={24} />,
+    },
+    {
+      key: "password",
+      label: "Password",
+      icon: <RiKey2Line className="text-gray-600" size={24} />,
+    },
+    {
+      key: "accountDetail",
+      label: "Account Detail",
+      icon: <HiOutlineUser className="text-gray-600" size={24} />,
+    },
+    {
+      key: "logout",
+      label: "Logout",
+      icon: <HiOutlineLogout className="text-gray-600" size={24} />,
+    },
+  ];
   return (
     <div className="flex px-40 h-screen">
-      <div className="w-1/4 border-r border-gray-300">
-        <ul className="space-y-8">
-          <li className="flex items-center space-x-3">
-            <FaShoppingCart className="text-gray-600" />
-            <span className="text-lg text-gray-800">Orders</span>
-          </li>
-          <li className="flex items-center space-x-3">
-            <FaHeart className="text-gray-600" />
-            <span className="text-lg text-gray-800">Wishlist</span>
-          </li>
-          <li className="flex items-center space-x-3">
-            <FaMapMarkerAlt className="text-gray-600" />
-            <span className="text-lg text-gray-800">Address</span>
-          </li>
-          <li className="flex items-center space-x-3">
-            <FaKey className="text-gray-600" />
-            <span className="text-lg text-gray-800">Password</span>
-          </li>
-          <li className="flex items-center space-x-3">
-            <FaUser className="text-gray-600" />
-            <span className="text-lg text-gray-800">Account Detail</span>
-          </li>
-          <li className="flex items-center space-x-3">
-            <FaSignOutAlt className="text-gray-600" />
-            <span className="text-lg text-gray-800">Logout</span>
-          </li>
+      <div className="w-1/5 mt-8">
+        <ul className="space-y-4">
+          {sections.map((section) => (
+            <li
+              key={section.key}
+              className={`flex items-center space-x-3 cursor-pointer p-3 rounded-lg ${
+                activeLink === section.key
+                  ? "bg-gray-200  w-[212px] "
+                  : "text-gray-800"
+              }`}
+              onClick={() => setActiveLink(section.key)}
+            >
+              {section.icon}
+              <span
+                className={`${
+                  activeLink === section.key
+                    ? "text-[#0E1422] font-semibold"
+                    : "text-[#5C5F6A]"
+                } text-lg`}
+              >
+                {section.label}
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
-      <div className="w-3/4 p-4">
+      <div className="w-2/5 p-4 border-l  border-gray-300">
         <h2 className="text-2xl font-bold mb-6">Orders</h2>
         <div className="space-y-4">
           {/* <div className="flex items-center justify-between p-4 border border-gray-300 rounded-lg"></div> */}
